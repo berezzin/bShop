b90464564e6aeb0f994fa65fad1617173a97bb33
berezzin <nexchanel@gmail.com>
2022-04-21 17:25:20 +0300

    Back to sqlite

--- bShop/bShop/settings.py
+++ bShop/bShop/settings.py
@@ -73,15 +73,15 @@ WSGI_APPLICATION = 'bShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bshopdb',
-        'USER': 'admin_bshop',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': 'admin_bshop',
+        # 'PASSWORD': 'root',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        # }
     }
 }
 
